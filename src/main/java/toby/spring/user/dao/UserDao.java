ca50453a6b7c3fce6bbe7ca1f268e88134230719
gmelon <hsh1769@naver.com>
2024-01-31 00:03:25 +0900

    refactor: 1.3.3 관계 설정 책임을 외부로 분리

--- src/main/java/toby/spring/user/dao/UserDao.java
+++ src/main/java/toby/spring/user/dao/UserDao.java
@@ -11,27 +11,8 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker;
 
-    public UserDao() {
-        this.connectionMaker = new DConnectionMaker();
-    }
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-
-        User user = new User();
-        user.setId("gmelon");
-        user.setName("현상혁");
-        user.setPassword("single");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user.getName());
-        System.out.println(user.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
