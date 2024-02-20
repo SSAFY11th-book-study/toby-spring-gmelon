3f65e8728658638662661842228517d086b8c207
gmelon <hsh1769@naver.com>
2024-02-20 23:57:26 +0900

    refactor: 2.2.2 UserDaoTest junit 기반으로 전환

--- src/main/java/toby/spring/user/dao/UserDaoTest.java
+++ src/main/java/toby/spring/user/dao/UserDaoTest.java
@@ -1,12 +1,18 @@
 package toby.spring.user.dao;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import java.sql.SQLException;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import toby.spring.user.domain.User;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
@@ -20,14 +26,11 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+    }
 
-        System.out.println(user2.getId() + " 조회 성공");
+    public static void main(String[] args) {
+        JUnitCore.main("toby.spring.user.dao.UserDaoTest");
     }
 }
