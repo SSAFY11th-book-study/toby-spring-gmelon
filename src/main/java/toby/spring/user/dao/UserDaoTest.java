8fb1688a59cc55214ee88fd635c2c875aa0499ba
gmelon <hsh1769@naver.com>
2024-02-20 23:43:45 +0900

    refactor: 2.2.1 테스트 결과를 코드를 통해 확인하도록 개선

--- src/main/java/toby/spring/user/dao/UserDaoTest.java
+++ src/main/java/toby/spring/user/dao/UserDaoTest.java
@@ -20,8 +20,13 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        System.out.println(user.getName());
-        System.out.println(user.getPassword());
+        if (!user.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if (!user.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
 
         System.out.println(user2.getId() + " 조회 성공");
     }
